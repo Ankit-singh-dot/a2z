2f59b9b20bd0171fbb4fce521dbb4118d1fe2309
Ankit <ankitrajdihara123@gmail.com>
2026-07-30 03:23:28 +0530

    learned_js: js/react.jsx

---
+++ js/react.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+function counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <h1>count</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increment{" "}
+      </button>
+    </>
+  );
+}
