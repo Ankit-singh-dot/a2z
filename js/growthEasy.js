fb29978b94629ffed5b4e6d47e6b3d49b16f2f92
Ankit <ankitrajdihara123@gmail.com>
2026-07-30 03:23:04 +0530

    learned_js: js/growthEasy.js

---
+++ js/growthEasy.js
@@ -0,0 +1,22 @@
+// for (let i = 1; i <= 5; i++) {
+//   console.log(`${i} - ${i * i}`);
+// }
+
+x = "44";
+y = 10;
+console.log(x + y);
+
+let one = "Coding";
+let two = "on";
+let three = "CodeChef";
+
+console.log(one + "-" + one.length);
+console.log(two + "-" + one.length);
+console.log(three + "-" + one.length);
+let sentence = one + " " + two + " " + three;
+console.log(sentence + " - " + sentence.length);
+console.log(false == 0);
+console.log(false === 0);
+console.log("" === 0);
+console.log(4 == "4");
+console.log(4 === "4");
