e9a44c210c5bc3fabc54fe500b5ff0d699a421c9
Ankit <ankitrajdihara123@gmail.com>
2026-07-30 03:23:06 +0530

    learned_js: js/hoisting.js

---
+++ js/hoisting.js
@@ -0,0 +1,12 @@
+getLog();
+console.log(x);
+getLog2();
+console.log(getLog);
+var x = 18;
+function getLog() {
+  console.log("Hello js");
+}
+
+var getLog2 = ()=>{
+    console.log("hello pppl")
+} 
