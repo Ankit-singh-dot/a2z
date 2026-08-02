9ac393fd761ed6717f761a6e3097842e45b3531e
Ankit <ankitrajdihara123@gmail.com>
2026-08-02 11:19:46 +0530

    seeting_up_cashing_expiry

--- js/new/testing_fast.js
+++ js/new/testing_fast.js
@@ -1,9 +1,15 @@
-import express from "express";
-
+import express, { json } from "express";
+import { createClient } from "redis";
+const redisClient = await createClient().connect();
 const app = express();
 app.get("/:id", async (req, res) => {
   try {
     const userId = req.params.id;
+    const cashedData = await redisClient.get(userId);
+    if (cashedData) {
+      console.log("redis hit");
+      return res.json(JSON.parse(cashedData));
+    }
     const response = await fetch(`https://fakestoreapi.com/products/${userId}`);
     if (!response.ok) {
       return res.status(response.status).json({
@@ -11,6 +17,9 @@ app.get("/:id", async (req, res) => {
       });
     }
     const data = await response.json();
+    await redisClient.set(userId, JSON.stringify(data), {
+        EX: 60,
+    });
     res.json(data);
   } catch (error) {
     res.status(500).json({
